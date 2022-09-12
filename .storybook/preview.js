e95cdc6ad37f3b6fa0867d1da0faf0a64537d8c8
shlomi-shavit <shlomishavit8@gmail.com>
2022-09-13 00:35:02 +0300

    Add files via upload

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import '../src/styles/typography.scss';
+import './storybook.scss';
+import { useTranslations } from '../src/translations/';
+
+useTranslations();
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
